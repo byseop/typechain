5be9c5e0a27fed298f277711ef9df0a76d6ee0eb
byseop <byseop@gmail.com>
2019-04-12 15:42:45 +0900

    #9. Creating a Block Part 3

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,38 @@
-import * as CryptoJS from "crypto-js";
+import * as CryptoJS from 'crypto-js';
 
 class Block {
-    public index: string;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-    static calculateBlockHash = (
-        index: number, 
-        previousHash: string, 
-        timestamp: number, 
-        data: string): string => 
-        CryptoJS.SHS256(index + previousHash + timestamp + data).toString();
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
-    constructor(
-        index: string,
-        hash: string,
-        previousHash: string,
-        data: string,
-        timestamp: number
-    ) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp;
-    }
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-Block.calculateBlockHash
+Block.calculateBlockHash;
 
-const genesisBlock:Block = new Block("0", "2020202020", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, '2020202020', '', 'Hello', 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
@@ -40,6 +41,27 @@ const getBlockchain = (): Block[] => blockchain;
 const getLastestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
-console.log(blockchain);
 
-export {}
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLastestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock('hello!'), createNewBlock('byebye'));
+
+export {};
