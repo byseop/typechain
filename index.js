aeb03c351419ef764a33c3667094269bea0b93db
byseop <byseop@gmail.com>
2019-04-12 11:53:57 +0900

    #3 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "baeyoungseop", age = 27, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + ", you are " + age + ", your are a " + gender);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+const name = "baeyoungseop",
+    age = 27,
+    gender = "male";
+
+
+// const sayHi = (name, age, gender?) => {  parameter 에 ? 를 붙이면 선택형이 됨
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, your are a ${gender}`);
+}
+
+// sayHi(name, age); 함수 선언시 parameter가 선택형이 아니면 모두 사용하여야 함 (error)
+sayHi(name, age, gender);
+
+export {};
